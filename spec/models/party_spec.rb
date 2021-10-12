16dbae6bd4c1eccffed7b62c1a113369b2419130
Andrew Massey <acmassey@ncsu.edu>
2021-10-12 18:09:26 -0400

    Model testing for relationships

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Party, type: :model do
-  describeq 'relationships' do
+  describe 'relationships' do
     it { should have_many(:attendees)}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'relationships' do
-    it { should have_many(:friends) }
+    it { should have_many(:friendships) }
     it { should have_many(:attendees) }
+    it { should have_many(:parties).through(:attendees)}
   end
 end
