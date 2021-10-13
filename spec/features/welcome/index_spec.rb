3653c1c5998b648627f33836e0f915b8c295f055
Andrew Massey <acmassey@ncsu.edu>
2021-10-12 20:52:03 -0400

    Create users controller and new action to serve as destination for register link

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe 'welcome page' do
     expect(page).to have_button("Sign In")
     expect(page).to have_link('New to Viewing Party? Register Here')
   end
+
+  it 'takes a user to the registration form' do
+    click_link("New to Viewing Party? Register Here")
+    expect(current_path).to eq(new_user_path)
+  end 
 end
