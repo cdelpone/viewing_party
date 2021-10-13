ce4217ff27e103a6a295309146040ea32017c437
Christina Delpone <cdelpone@gmail.com>
2021-10-13 16:43:17 -0600

    Add current and require user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class DashboardController < ApplicationController
+  before_action :require_user
+
   def index
-  end 
+  end
+
+  private
+  def require_user
+    if !current_user
+      flash[:alert] = "You shall not pass"
+      redirect_to root_path
+    end
+  end
 end
