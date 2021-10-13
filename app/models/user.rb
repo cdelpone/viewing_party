1e728bb5465a347c83d9f4f72d34ea3c31267dcd
Andrew Massey <acmassey@ncsu.edu>
2021-10-12 21:50:18 -0400

    Create and test more validatiaons for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :attendees
   has_many :parties, through: :attendees
+
+  validates :email, presence:true, uniqueness: true
   validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, on: :create
   has_secure_password
 end
