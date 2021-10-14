9da904a4f2f7a821ee63e8affce616f042321165
Christina Delpone <cdelpone@gmail.com>
2021-10-14 16:35:30 -0600

    Test search by email class method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password_confirmation) }
     it { should validate_confirmation_of(:password).on(:create)}
   end
+
+  describe 'class methods' do
+    describe 'search by email' do
+      it 'can search by email' do
+        python = User.create!(email: "python@pythonmail.com", password: "potato", password_confirmation: "potato")
+        java   = User.create!(email: "java@javamail.com", password: "banana", password_confirmation: "banana")
+        user = User.search_by_email(python.email)
+
+        expect(user).to eq(python)
+      end
+    end
+  end
 end
