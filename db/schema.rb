f67b10c340be5b7bd25a43961c39e857a1641b39
Andrew Massey <acmassey@ncsu.edu>
2021-10-15 19:05:24 -0400

    Create new column on the party table for movie_title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_135222) do
+ActiveRecord::Schema.define(version: 2021_10_15_220251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_10_15_135222) do
   create_table "parties", force: :cascade do |t|
     t.integer "movie_id"
     t.datetime "date"
+    t.string "movie_title"
   end
 
   create_table "users", force: :cascade do |t|
