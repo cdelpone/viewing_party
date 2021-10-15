47fbdcdc15f3cfc45b82e02c3c6f7ad6ce0ae682
Christina Delpone <cdelpone@gmail.com>
2021-10-14 19:32:19 -0600

    Index actions renders sad happy paths with friends

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,16 @@ class DashboardController < ApplicationController
   before_action :require_user
 
   def index
+    if params[:email].present?
+      friend = User.search_by_email(params[:email])
+      if current_user.friends.include?(friend)
+        flash[:alert] = "Ya'll are already friends."
+      elsif friend
+        current_user.friendships.create(friend: friend)
+        current_user.reload
+      else
+        flash[:alert] = "Sorry, that's an imaginary friend."
+      end
+    end
   end
 end
