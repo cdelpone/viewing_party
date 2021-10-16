678152667f09c5cbf9556a007f491b9c8194ce26
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-15 18:09:56 -0600

    Add tests for Party model, refactor all methods
    Co-authored-by: Christina Delpone <81711519+cdelpone@users.noreply.github.com>
    Co-authored-by: Andrew Massey <75844153+acmassey3698@users.noreply.github.com>

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,15 +2,13 @@ class Party < ApplicationRecord
   has_many :attendees
 
   def host?(user)
-    attendee = attendees.where(user_id: user.id).first
-    attendee && attendee.role == "host"
+    user == host
   end
 
   def host
-    attendees.joins(:user)
-             .select('users.email')
-             .where(role: 0)
-             .first
+    User.joins(:attendees)
+        .where(attendees: {role: 0})
+        .first
   end
 
   def host_email
@@ -18,8 +16,7 @@ class Party < ApplicationRecord
   end
 
   def invited_guests
-    attendees.joins(:user)
-             .select('users.email, attendees.*')
-             .where(role: 1)
+    User.joins(:attendees)
+        .where(attendees: {role: 1})
   end
 end
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -4,4 +4,44 @@ RSpec.describe Party, type: :model do
   describe 'relationships' do
     it { should have_many(:attendees)}
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @ruby   = User.create(email: "ruby@gmail.com", password: "potato", password_confirmation: "potato")
+      @java   = User.create!(email: "java@javamail.com", password: "banana", password_confirmation: "banana")
+      @python = User.create!(email: "python@pythonmail.com", password: "potato", password_confirmation: "potato")
+
+      @sw = @ruby.parties.create!(date: "2018-01-02 04:30:00 UST", movie_id: 1, movie_title: "Star Wars")
+
+      @java.attendees.create(party: @sw, role: 1)
+    end
+
+    describe '#host?(user)' do
+      it 'confirms if user is host' do
+        expect(@sw.host?(@ruby)).to be_truthy
+        expect(@sw.host?(@java)).to be_falsy
+      end
+    end
+
+    describe '#host' do
+      it 'returns the host' do
+        expect(@sw.host).to     eq(@ruby)
+        expect(@sw.host).to_not eq(@java)
+      end
+    end
+
+    describe '#host_email' do
+      it 'returns host email' do
+        expect(@sw.host_email).to     eq(@ruby.email)
+        expect(@sw.host_email).to_not eq(@java.email)
+      end
+    end
+
+    describe '#invited_guests' do
+      it 'returns invited guests excluding host' do
+        expectation = [@java]
+        expect(@sw.invited_guests).to eq(expectation)
+      end
+    end
+  end
 end
