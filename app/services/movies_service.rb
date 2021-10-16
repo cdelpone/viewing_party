d41b443fa46c80ff0ae867c790b4a499e3afecf2
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-15 20:31:18 -0600

    Fix stub, movies good, make poros next

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -14,25 +14,11 @@ class MoviesService
 
   def top_40_movies
     movie_data_page_1 = get_data('movie/top_rated')
-    # movie_data_page_2 = get_data('movie/top_rated?page=2')
-    # results = movie_data_page_1[:results]
-    # movie_data_page_2[:results].each do |result|
-    #   results << result
-    # end
-    # results
+    movie_data_page_2 = get_data('movie/top_rated?page=2')
+    results = movie_data_page_1[:results]
+    movie_data_page_2[:results].each do |result|
+      results << result
+    end
+    results
   end
 end
-
-
-# conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-#       faraday.headers["X-API-KEY"] = ENV['govt_api_key']
-#     end
-#     response      = conn.get("/congress/v1/116/senate/members.json")
-#
-#     data          = JSON.parse(response.body, symbolize_names: true)
-#
-#     members       = data[:results][0][:members]
-#
-#     found_members = members.find_all {|m| m[:last_name] == params[:search]}
-#     @member       = found_members.first
-#     render "welcome/index"
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -18,26 +18,22 @@ RSpec.describe 'movies index', type: :feature do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      visit movies_path
     end
 
     it 'lists 40 titles of top movies' do
       json_response_1 = File.read('spec/fixtures/top_40_movies_1.json')
-      # json_response_2 = File.read('spec/fixtures/top_40_movies_2.json')
+      json_response_2 = File.read('spec/fixtures/top_40_movies_2.json')
 
-      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
-        with(params: {'api_key'=> ENV['movie_key']}).
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_key']}").
         to_return(status: 200, body: json_response_1, headers: {})
-      # stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_key']}&page=2").
-      #   to_return(status: 200, body: json_response_2, headers: {})
+
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_key']}&page=2").
+        to_return(status: 200, body: json_response_2, headers: {})
+
+      visit movies_path
 
       expect(page).to have_content("Dilwale Dulhania Le Jayenge")
-      # expect(page).to have_content("Life in a Year")
+      expect(page).to have_content("Life in a Year")
     end
   end
 end
-
-# json_response = File.read('spec/fixtures/members_of_the_senate.json')
-#         stub_request(:get, "https://api.propublica.org/congress/v1/116/senate/members.json").
-#           with(headers: {'X-Api-Key'=> ENV['govt_api_key']}).
-#           to_return(status: 200, body: json_response, headers: {})
