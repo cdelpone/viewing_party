c96ce69da651119e0a3ef27fc1279da9f6ff5024
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-15 20:20:23 -0600

    API sandbox

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 class MoviesController < ApplicationController
   before_action :require_user
-  
+
   def index
+    movie_service = MoviesService.new
+    @movies = movie_service.top_40_movies
+    binding.pry
   end
 end
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -21,8 +21,23 @@ RSpec.describe 'movies index', type: :feature do
       visit movies_path
     end
 
-    it 'lists 40 titles of top movies', :vcr do
-      
+    it 'lists 40 titles of top movies' do
+      json_response_1 = File.read('spec/fixtures/top_40_movies_1.json')
+      # json_response_2 = File.read('spec/fixtures/top_40_movies_2.json')
+
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
+        with(params: {'api_key'=> ENV['movie_key']}).
+        to_return(status: 200, body: json_response_1, headers: {})
+      # stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_key']}&page=2").
+      #   to_return(status: 200, body: json_response_2, headers: {})
+
+      expect(page).to have_content("Dilwale Dulhania Le Jayenge")
+      # expect(page).to have_content("Life in a Year")
     end
   end
 end
+
+# json_response = File.read('spec/fixtures/members_of_the_senate.json')
+#         stub_request(:get, "https://api.propublica.org/congress/v1/116/senate/members.json").
+#           with(headers: {'X-Api-Key'=> ENV['govt_api_key']}).
+#           to_return(status: 200, body: json_response, headers: {})
