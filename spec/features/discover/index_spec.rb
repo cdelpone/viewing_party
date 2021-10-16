6a23c53a91657b3d250bd0a7686a11eb381523b3
Andrew Massey <acmassey@ncsu.edu>
2021-10-16 17:39:55 -0400

    Create and test the discovers#index page

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -8,4 +8,46 @@ RSpec.describe 'discover index', type: :feature do
       expect(page).to have_content("You shall not pass")
     end
   end
+
+  describe 'valid user' do
+    before :each do
+      @user = User.create!(email: "ruby@rubymail.com", password: "turing", password_confirmation: "turing")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      json_response_1 = File.read('spec/fixtures/top_40_movies_1.json')
+      json_response_2 = File.read('spec/fixtures/top_40_movies_2.json')
+
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_key']}").
+      to_return(status: 200, body: json_response_1, headers: {})
+
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_key']}&page=2").
+      to_return(status: 200, body: json_response_2, headers: {})
+
+      fc_response_1 = File.read('spec/fixtures/fight_club_1.json')
+      fc_response_2 = File.read('spec/fixtures/fight_club_2.json')
+
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_key']}&query=Fight Club").
+      to_return(status: 200, body: fc_response_1, headers: {})
+
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_key']}&query=Fight Club&page=2").
+      to_return(status: 200, body: fc_response_2, headers: {})
+
+      visit discover_path
+    end
+
+    it 'has a button for find top movies' do
+      expect(page).to have_button('Find Top Movies')
+      click_button('Find Top Movies')
+      expect(current_path).to eq(movies_path)
+    end
+
+    it 'has a movie search form' do
+      fill_in(:movie_title, with: 'Fight Club')
+      click_button('Find Movies')
+      expect(current_path).to eq(movies_path)
+      expect(page).to have_content('Fight Club')
+      expect(page).to have_content('Barrio Brawler')
+    end
+  end
 end
