84d184fc975fcb352abff5e6b8785874247a7abc
Andrew Massey <acmassey@ncsu.edu>
2021-10-16 18:49:12 -0400

    Add presentation methods for movie poro

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,7 +1,9 @@
 class Movie
   attr_reader :adult,
               :backdrop_path,
+              :runtime,
               :genre_ids,
+              :genres,
               :id,
               :original_language,
               :original_title,
@@ -16,8 +18,10 @@ class Movie
 
   def initialize(data)
     @adult             = data[:adult]
+    @runtime           = data[:runtime]
     @backdrop_path     = data[:backdrop_path]
     @genre_ids         = data[:genre_ids]
+    @genres            = data[:genres]
     @id                = data[:id]
     @original_language = data[:original_language]
     @original_title    = data[:original_title]
@@ -29,5 +33,17 @@ class Movie
     @videos            = data[:videos]
     @vote_average      = data[:vote_average]
     @vote_count        = data[:vote_count]
+  end
+
+  def genre_names
+    genres.map do |genre|
+      genre[:name]
+    end
+  end
+
+  def formatted_runtime
+    hours = @runtime / 60
+    rest = @runtime % 60
+    "#{hours} hour(s) #{rest} minute(s)"
   end
 end
