3a15fcca76d0476e33c339e4f81eb9f03d91bf4c
Andrew Massey <acmassey@ncsu.edu>
2021-10-17 12:45:30 -0400

    Create form for a new party and controller action

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,8 @@
 class PartiesController < ApplicationController
   before_action :require_user
-  def new 
+  def new
+    @party = Party.new
+    @movie_title = params[:movie_title]
+    @movie_runtime = params[:runtime].to_i
   end
 end
