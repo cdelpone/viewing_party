63c95dff5da15fbd43ba3c127c72cc4b0be64edb
Andrew Massey <acmassey@ncsu.edu>
2021-10-17 11:24:17 -0400

    Add timestamps and duration to parties table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_220251) do
+ActiveRecord::Schema.define(version: 2021_10_17_152026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 2021_10_15_220251) do
     t.integer "movie_id"
     t.datetime "date"
     t.string "movie_title"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
