ae0966113230f7e67e7913f2ddf77b6cfbfdc055
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-17 12:55:27 -0600

    Make the form add a new party and friends to the guest list

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,8 +1,29 @@
 class PartiesController < ApplicationController
   before_action :require_user
   def new
-    @party = Party.new
-    @movie_title = params[:movie_title]
+    @party         = Party.new
+    @movie_title   = params[:movie_title]
     @movie_runtime = params[:runtime].to_i
+    @movie_id      = params[:movie_id]
+  end
+
+  def create
+    party = current_user.parties.create(party_params)
+    party.invite_friends_by_ids(included_friend_ids)
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def party_params
+    params.require(:party)
+          .permit(:duration, :date, :time, :movie_id, :movie_title)
+  end
+
+  def included_friend_ids
+    keys = params[:party].keys
+    keys.find_all do |friend_id|
+      friend_id.to_i > 0 && params[:party][friend_id] == '1'
+    end
   end
 end
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe 'New Party Form' do
 
   describe 'valid user' do
     before :each do
-      @user = User.create!(email: "ruby@rubymail.com", password: "turing", password_confirmation: "turing")
+      @ruby = User.create!(email: "ruby@rubymail.com", password: "turing", password_confirmation: "turing")
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@ruby)
 
       @movie_data = File.read('spec/fixtures/guardians_info.json')
 
@@ -31,21 +31,22 @@ RSpec.describe 'New Party Form' do
       to_return(status: 200, body: @reviews, headers: {})
 
       @parsed_data = JSON.parse(@movie_data)
+      @movie_title = @parsed_data['title']
 
       @python = User.create!(email: "python@pythonmail.com", password: "potato", password_confirmation: "potato")
       @java   = User.create!(email: "java@javamail.com", password: "banana", password_confirmation: "banana")
-      @ruby   = User.create(email: "ruby@gmail.com", password: "potato", password_confirmation: "potato")
+      @yaml   = User.create(email: "yaml@gmail.com", password: "potato", password_confirmation: "potato")
 
-      @user.friendships.create!(friend: @python)
-      @user.friendships.create!(friend: @java)
-      @user.friendships.create!(friend: @ruby)
+      @ruby.friendships.create!(friend: @python)
+      @ruby.friendships.create!(friend: @java)
+      @ruby.friendships.create!(friend: @yaml)
 
       visit(movie_path(118340))
       click_button("Create Viewing Party")
     end
 
     it 'has the movie title above the form' do
-      expect(page).to have_content(@parsed_data['title'])
+      expect(page).to have_content(@movie_title)
     end
 
     it 'has all appropriate fields' do
@@ -53,9 +54,25 @@ RSpec.describe 'New Party Form' do
       expect(page).to have_field('party[date]')
       expect(page).to have_field('party[time]')
       expect(page).to have_field("party[#{@python.id}]")
-      expect(page).to have_field("party[#{@ruby.id}]")
+      expect(page).to have_field("party[#{@yaml.id}]")
       expect(page).to have_field("party[#{@java.id}]")
     end
-  end
 
+    it 'can make a party' do
+      fill_in('party[duration]', with: 180)
+      fill_in('party[date]', with: "2018-01-02")
+      fill_in('party[time]', with: "04:30:00 UST")
+      check("party[#{@python.id}]")
+      check("party[#{@yaml.id}]")
+      click_button("Create Party")
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_link(@movie_title)
+      expect(page).to have_content("Tuesday, January 2, 2018 at 04:30AM")
+      expect(page).to have_content("Hosting")
+      expect(page).to have_content("Attending:")
+      expect(page).to have_content(@python.email)
+      expect(page).to have_content(@yaml.email)
+    end
+  end
 end
