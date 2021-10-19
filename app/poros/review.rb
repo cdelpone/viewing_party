d118bb76a0a75aba63c1a910d1d89fab243a9fbf
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-19 15:14:04 -0600

    Remove unneeded attributes, change param name

--- app/poros/review.rb
+++ app/poros/review.rb
@@ -1,19 +1,9 @@
 class Review
   attr_reader :author,
-              :author_details,
-              :content,
-              :created_at,
-              :id,
-              :updated_at,
-              :url
+              :content
 
-  def initialize(info)
-    @author = info[:author]
-    @author_details = info[:author_details]
-    @content = info[:content]
-    @created_at = info[:created_at]
-    @id = info[:id]
-    @updated_at = info[:updated_at]
-    @url = info[:url]
+  def initialize(data)
+    @author         = data[:author]
+    @content        = data[:content]
   end
 end
