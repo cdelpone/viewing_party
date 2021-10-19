e7ea0612d97423ecf1c522aa2c25c6ac86e8beaa
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-19 14:37:14 -0600

    Add return clause, helper method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,16 +2,21 @@ class DashboardController < ApplicationController
   before_action :require_user
 
   def index
-    if params[:email].present?
-      friend = User.search_by_email(params[:email])
-      if current_user.friends.include?(friend)
-        flash[:alert] = "Ya'll are already friends."
-      elsif friend
-        current_user.friendships.create(friend: friend)
-        # current_user.reload
-      else
-        flash[:alert] = "Sorry, that's an imaginary friend."
-      end
+    return if params[:email].blank?
+
+    if current_user.friends.include?(friend)
+      flash[:alert] = "Ya'll are already friends."
+    elsif friend
+      current_user.friendships.create(friend: friend)
+      # current_user.reload
+    else
+      flash[:alert] = "Sorry, that's an imaginary friend."
     end
+  end
+
+  private
+
+  def friend
+    User.search_by_email(params[:email])
   end
 end
