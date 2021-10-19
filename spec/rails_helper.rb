c34472c83db016e6dba5c89a21c0516077579169
Andrew Massey <acmassey@ncsu.edu>
2021-10-18 22:24:35 -0400

    Add VCR configuration to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,11 +69,11 @@ RSpec.configure do |config|
     end
   end
 
-  # VCR.configure do |config|
-  #   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  #   config.hook_into :webmock
-  #   config.filter_sensitive_data('api_key') { ENV['movie_key'] }
-  #   config.configure_rspec_metadata!
-  #   config.default_cassette_options = { re_record_interval: 7.days }
-  # end
+  VCR.configure do |config|
+    config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+    config.hook_into :webmock
+    config.filter_sensitive_data('<api_key>') { ENV['movie_key'] }
+    config.configure_rspec_metadata!
+    config.default_cassette_options = { re_record_interval: 7.days }
+  end
 end
