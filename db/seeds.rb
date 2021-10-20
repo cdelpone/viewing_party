f7694c78dd117552d9f3532ee6bcdc5c3caffe77
Andrew Massey <acmassey@ncsu.edu>
2021-10-20 19:22:37 -0400

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Friendship.destroy_all
+User.destroy_all
+
+ruby = User.create(email: 'ruby@rubymail.com', password: 'ruby', password_confirmation: 'ruby')
+python = User.create(email: 'python@pythonmail.com', password: 'python', password_confirmation: 'python')
+java = User.create(email: 'java@javamail.com', password: 'java', password_confirmation: 'java')
+node = User.create(email: 'node@nodemail.com', password: 'node', password_confirmation: 'node')
+react = User.create(email: 'react@reactmail.com', password: 'react', password_confirmation: 'react')
+css = User.create(email: 'css@cssmail.com', password: 'css', password_confirmation: 'css')
+sql = User.create(email: 'sql@sqlmail.com', password: 'sql', password_confirmation: 'sql')
+linux = User.create(email: 'linux@linuxmail.com', password: 'linux', password_confirmation: 'linux')
+
+ruby.friendships.create(friend: python)
+ruby.friendships.create(friend: java)
+ruby.friendships.create(friend: node)
+ruby.friendships.create(friend: react)
+ruby.friendships.create(friend: css)
+ruby.friendships.create(friend: sql)
+ruby.friendships.create(friend: linux)
+
+python.friendships.create(friend: java)
+python.friendships.create(friend: node)
+python.friendships.create(friend: react)
+python.friendships.create(friend: css)
+python.friendships.create(friend: sql)
+python.friendships.create(friend: linux)
+
+java.friendships.create(friend: node)
+java.friendships.create(friend: react)
+java.friendships.create(friend: css)
+java.friendships.create(friend: sql)
+java.friendships.create(friend: linux)
+
+node.friendships.create(friend: react)
+node.friendships.create(friend: css)
+node.friendships.create(friend: sql)
+node.friendships.create(friend: linux)
+
+react.friendships.create(friend: css)
+react.friendships.create(friend: sql)
+react.friendships.create(friend: linux)
+
+css.friendships.create(friend: sql)
+css.friendships.create(friend: linux)
+
+sql.friendships.create(friend: linux)
+
+party = ruby.parties.create(movie_id: 118340, movie_title: "Guardians of the Galaxy", duration: 200, date: "2022-01-02", time: "04:30:00 UST")
+
+party.invite_friends_by_ids([python.id, node.id, java.id, react.id, css.id, sql.id, linux.id])
